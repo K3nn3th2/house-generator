eb6fceead3cb24e37beef8cdc2f6f8bd99efb91a
K3nn3th2 <kallan@gmx.net>
2023-11-06 16:12:47 +0100

    switched to rendering with geometry shader
    - all vertices representing a house are now generated in the geometry shader.
    - WASD keys allow movement around
    - added camera and shader class for convenience

---
+++ geometryShader.geom
@@ -0,0 +1,79 @@
+#version 330
+
+layout(points) in;
+layout(triangle_strip, max_vertices = 50) out;
+
+out vec3 fColour;
+
+uniform mat4 u_mvp;
+
+uniform vec4 u_location;
+
+uniform float u_length;
+uniform float u_width;
+uniform float u_height_roof;
+uniform float u_height_wall;
+
+uniform float u_height_clip;
+
+uniform vec3 u_colour_roof;
+uniform vec3 u_colour_wall;
+
+
+void handleVertex(vec4 pos){
+   gl_Position = u_mvp * pos;
+   gl_ClipDistance[0] = dot(pos, vec4(0.0, -1.0, 0.0, u_height_clip));
+   EmitVertex();
+
+}
+
+void build_house(vec4 position){
+   // front side
+   fColour = u_colour_wall;
+   handleVertex(position + vec4(-u_width/2, 0.0, -u_length/2, 0.0)); // 1: bottom left
+   handleVertex(position + vec4(u_width/2, 0.0, -u_length/2, 0.0)); // 2: bottom-right
+   handleVertex(position + vec4(-u_width/2, u_height_wall, -u_length/2, 0.0)); // 3: top left
+   handleVertex(position + vec4(u_width/2, u_height_wall, -u_length/2, 0.0)); // 4: top right
+   handleVertex(position + vec4(0, u_height_roof, -u_length/2, 0.0)); // 5: top
+   EndPrimitive();
+
+   // right side
+   handleVertex(position + vec4(u_width/2, 0.0, -u_length/2, 0.0)); // 1: bottom right
+   handleVertex(position + vec4(u_width/2, 0.0, u_length/2, 0.0)); // 2: back bottom right
+   handleVertex(position + vec4(u_width/2, u_height_wall, -u_length/2, 0.0)); // 4: top right
+   handleVertex(position + vec4(u_width/2, u_height_wall, u_length/2, 0.0)); // 4: back top right
+   EndPrimitive();
+
+   // back side
+   handleVertex(position + vec4(u_width/2, 0.0, u_length/2, 0.0)); // 2: bottom-right
+   handleVertex(position + vec4(-u_width/2, 0.0, u_length/2, 0.0)); // 1: bottom left
+   handleVertex(position + vec4(u_width/2, u_height_wall, u_length/2, 0.0)); // 4: top right
+   handleVertex(position + vec4(-u_width/2, u_height_wall, u_length/2, 0.0)); // 3: top left
+   handleVertex(position + vec4(0, u_height_roof, u_length/2, 0.0)); // 5: top
+   EndPrimitive();
+
+   // left side
+   handleVertex(position + vec4(-u_width/2, 0.0, -u_length/2, 0.0)); // 1: bottom left
+   handleVertex(position + vec4(-u_width/2, u_height_wall, -u_length/2, 0.0)); // 4: top left
+   handleVertex(position + vec4(-u_width/2, 0.0, u_length/2, 0.0)); // 2: back bottom left
+   handleVertex(position + vec4(-u_width/2, u_height_wall, u_length/2, 0.0)); // 4: back top left
+   EndPrimitive();
+
+   // roof
+   fColour = u_colour_roof;
+   handleVertex(position + vec4(u_width/2, u_height_wall, -u_length/2, 0.0)); // 1: top right
+   handleVertex(position + vec4(u_width/2, u_height_wall, u_length/2, 0.0)); // 2: top back right
+   handleVertex(position + vec4(0, u_height_roof, -u_length/2, 0.0)); // 3: top front
+   handleVertex(position + vec4(0, u_height_roof, u_length/2, 0.0)); // 4: top back
+   handleVertex(position + vec4(-u_width/2, u_height_wall, -u_length/2, 0.0)); // 5: top left
+   handleVertex(position + vec4(-u_width/2, u_height_wall, u_length/2, 0.0)); // 6: top back left
+   EndPrimitive();
+}
+
+void main() {
+   
+   build_house(u_location);
+
+}
+
+
--- house.cpp
+++ house.cpp
@@ -1,77 +1,81 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "shader.h"
+#include "camera.h"
 #include <iostream>
 #include <random>
 #include <vector>
-#include <chrono>
+//#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
-/* This Program displays a random number of houses in OpenGL.
- * These houses appear to "grow" out of the ground and disappear again, repeatedly.
- * The animation cycle takes one second to let all of the houses appear completely (the highest one will take one second to be fully displayed), displays them for three seconds, and lets them disappear again within one second.
- * In between the cycles, all of the houses are assigned new random dimensional attribute values which are displayed in the next cycle.
-*/
 
 const int WIDTH = 1920;
 const int HEIGHT = 1080;
 
-const double MAX_POS_X = 10.0;
-const double MAX_POS_Z = 10.0;
+const int MAX_HOUSES = 50;
 
 // dimensional constraints for the houses
 
-const double MIN_HOUSE_WIDTH = 0.5;
-const double MIN_HOUSE_LENGTH = 0.5;
-const double MIN_HOUSE_HEIGHT_WALL = 0.5;
+const float MAX_POS_X = 25.0;
+const float MAX_POS_Z = 25.0;
 
-const double MAX_HOUSE_WIDTH = 2.5;
-const double MAX_HOUSE_LENGTH = 2.5;
-const double MAX_HOUSE_HEIGHT_ROOF = 3.5;
-const double MAX_HOUSE_HEIGHT_WALL = 2.5;
+const float MIN_HOUSE_WIDTH = 1.5;
+const float MIN_HOUSE_LENGTH = 1.5;
+const float MIN_HOUSE_HEIGHT_WALL = 1.5;
 
-double house_highest = 0.0;
-    
+const float MAX_HOUSE_WIDTH = 3.5;
+const float MAX_HOUSE_LENGTH = 7.5;
+const float MAX_HOUSE_HEIGHT_ROOF = 7.5;
+const float MAX_HOUSE_HEIGHT_WALL = 4.0;
+
+float house_highest = 0.0;
 
 // initialize random number generator
 std::random_device rd;
 std::mt19937 gen(rd());
 
 
-double random_val(double min, double max){
-    std::uniform_real_distribution<double> dist(min, max);
-    return dist(gen);
-}
+// camera
+Camera camera(glm::vec3(25.0f, 10.0f, 25.0f));
+glm::mat4 mat_projection = glm::perspective(glm::radians(80.0f), (float)WIDTH / (float)HEIGHT, 1.0f, 150.0f);
 
-/* The class used to hold data regarding a house
- * It holds the dimensional and positional attributes as well as the RGB channel values for the roof and the walls of the house.
- * It contains a method which sets the dimensional attributes to random numbers.
-*/
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+float startFrame = 0.0f;
+float currentFrame = 0.0;
+float duration = 0.0;
+float ratio = 0.0;
+
+float random_val(float min, float max){
+    std::uniform_real_distribution<float> dist(min, max);
+    float generated = dist(gen);
+    return generated;
+}
 
 class House{
     
     public:
-        double height_roof, height_wall, width, length;
-        double pos_x, pos_z;
-        double pos_y = 0.0; //on ground level
-        double color_wall_r, color_wall_g, color_wall_b;
-        double color_roof_r, color_roof_g, color_roof_b;
+        float height_roof, height_wall, width, length;
+        float pos_x, pos_z;
+        float pos_y = 0.0; //on ground level
+        float colour_wall_r, colour_wall_g, colour_wall_b;
+        float colour_roof_r, colour_roof_g, colour_roof_b;
+        glm::mat4 mat_model = glm::mat4(1.0f);
 
         House(){
            pos_x = random_val(-MAX_POS_X, MAX_POS_X);
            pos_z = random_val(-MAX_POS_Z, MAX_POS_Z);
-            
            set_random_dimensions();
-
-           color_wall_r = random_val(0.0, 1.0);
-           color_wall_g = random_val(0.0, 1.0);
-           color_wall_b = random_val(0.0, 1.0);
-
-           color_roof_r = random_val(0.0, 1.0);
-           color_roof_g = random_val(0.0, 1.0);
-           color_roof_b = random_val(0.0, 1.0);
+           set_random_colours();
         }
 
         // specific constructor in case needed
-        House(double x, double y, double z, double w, double l, double h_r, double h_w, double col_wall_r, double col_wall_g, double col_wall_b, double col_roof_r, double col_roof_g, double col_roof_b){
+        House(float x, float y, float z, float w, float l, float h_r, float h_w, float col_wall_r, float col_wall_g, float col_wall_b, float col_roof_r, float col_roof_g, float col_roof_b){
            pos_x = x;
            pos_z = z;
 
@@ -80,16 +84,17 @@ class House{
            height_roof = h_r;
            height_wall = h_w;
 
-           color_wall_r = col_wall_r;
-           color_wall_g = col_wall_g;
-           color_wall_b = col_wall_b;
-           color_roof_r = col_roof_r;
-           color_roof_g = col_roof_g;
-           color_roof_b = col_roof_b;
+           colour_wall_r = col_wall_r;
+           colour_wall_g = col_wall_g;
+           colour_wall_b = col_wall_b;
+           colour_roof_r = col_roof_r;
+           colour_roof_g = col_roof_g;
+           colour_roof_b = col_roof_b;
         }
 
         // This method sets the dimensional attributes to random values within the specified boundaries.
         void set_random_dimensions(){
+           //mat_model = glm::rotate(glm::mat4(1.0f), (float)glm::radians(random_val(0.0, 359.0)), glm::vec3(0.0, 1.0, 0.0));
            width = random_val(MIN_HOUSE_WIDTH, MAX_HOUSE_WIDTH);
            length = random_val(MIN_HOUSE_LENGTH, MAX_HOUSE_LENGTH);
            height_wall = random_val(MIN_HOUSE_HEIGHT_WALL, MAX_HOUSE_HEIGHT_WALL);
@@ -99,162 +104,121 @@ class House{
                house_highest = height_roof;
         }
 
-        void draw_roof(){
-            glBegin(GL_TRIANGLE_STRIP);
-            glColor3f(color_roof_r, color_roof_g, color_roof_b); // roof color
-                                                                 //
-            // front left top
-            glVertex3f(pos_x - width/2, pos_y + height_wall, pos_z - length/2);
-            // back right top
-            glVertex3f(pos_x - width/2, pos_y + height_wall, pos_z + length/2);
-            // front roof top
-            glVertex3f(pos_x, pos_y + height_roof, pos_z - length/2);
-            // back roof top
-            glVertex3f(pos_x, pos_y + height_roof, pos_z + length/2);
-            // front right top
-            glVertex3f(pos_x + width/2, pos_y + height_wall, pos_z - length/2);
-            // back right top
-            glVertex3f(pos_x + width/2, pos_y + height_wall, pos_z + length/2);
-
-            glEnd();
-
-        }
-
-        void draw_walls(){
-            glBegin(GL_TRIANGLE_STRIP);
-
-            glColor3f(color_wall_r, color_wall_g, color_wall_b); // wall color
-                                                                 
-            // front left top
-            glVertex3f(pos_x - width/2, pos_y + height_wall, pos_z - length/2);
-            // front right bottom
-            glVertex3f(pos_x + width/2, pos_y, pos_z - length/2 );
-            // front right top
-            glVertex3f(pos_x + width/2, pos_y + height_wall, pos_z - length/2);
-            // back left bottom
-            glVertex3f(pos_x + width/2, pos_y, pos_z + length/2);
-            // back left top
-            glVertex3f(pos_x + width/2, pos_y + height_wall, pos_z + length/2);
-            // back right bottom
-            glVertex3f(pos_x - width/2, pos_y, pos_z + length/2);
-            // back right top
-            glVertex3f(pos_x - width/2, pos_y + height_wall, pos_z + length/2);
-            // front left bottom
-            glVertex3f(pos_x - width/2, pos_y, pos_z - length/2);
-            // front left top
-            glVertex3f(pos_x - width/2, pos_y + height_wall, pos_z - length/2);
-            // front right bottom
-            glVertex3f(pos_x + width/2, pos_y, pos_z - length/2);
-
-            glEnd();
-            glBegin(GL_TRIANGLES);
-            // front left top
-            glVertex3f(pos_x - width/2, pos_y + height_wall, pos_z - length/2);
-            // front right top
-            glVertex3f(pos_x + width/2, pos_y + height_wall, pos_z - length/2);
-            // front roof top
-            glVertex3f(pos_x, pos_y + height_roof, pos_z - length/2);
-            
-            // front left top
-            glVertex3f(pos_x - width/2, pos_y + height_wall, pos_z + length/2);
-            // front right top
-            glVertex3f(pos_x + width/2, pos_y + height_wall, pos_z + length/2);
-            // back roof top
-            glVertex3f(pos_x, pos_y + height_roof, pos_z + length/2);
-
-            glEnd();
-
-        }
-        void draw(){
-            draw_walls();
-            draw_roof();
+        void set_random_colours(){
+           colour_wall_r = random_val(0.0, 1.0);
+           colour_wall_g = random_val(0.0, 1.0);
+           colour_wall_b = random_val(0.0, 1.0);
+           colour_roof_r = random_val(0.0, 1.0);
+           colour_roof_g = random_val(0.0, 1.0);
+           colour_roof_b = random_val(0.0, 1.0);
         }
 
 };
 
-void render(std::vector<House*> houses) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    for (House* house : houses){
-        house->draw();
-    }
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+double get_clip_height(){
+     double height_clip = house_highest;
+     if (duration <= 1.0){ // increase clipping plane height.
+         ratio = duration;
+         height_clip = ratio * house_highest;
+
+     }else if(duration >= 5.0){ // total animation cycle is over. reset clip plane height and timer 
+         height_clip = 0.0;
+         std::cout << "Resetting animation." << std::endl; 
+         startFrame = static_cast<float>(glfwGetTime());
+
+     }else if(duration >= 4.0){ // lower the clipping plane.
+         ratio = (duration - 4.0);
+         double dist = house_highest - ratio * house_highest;
+         height_clip = dist;
+     }
+     return height_clip;
+
 }
 
 int main() {
+    // Initialize GLFW and GLEW
+    glfwInit();
 
-    if (!glfwInit()) {
-        std::cout << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
-
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Parametric House", NULL, NULL);
-    if (!window) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
+    // Create a window
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Parametric House Renderer", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
-    if (glewInit() != GLEW_OK) {
-        std::cout << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }
-    glEnable(GL_DEPTH_TEST);
-    // Set up projection matrix
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-10.0, 10.0, -10.0, 10.0, -10.0, 100.0);
-
-    // Set up modelview matrix
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(10.0, 10.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-
     std::vector<House*> houses;
-    int amount_houses = (int)random_val(12.0, 35.0);
+    int amount_houses = (int)random_val(12.0, (double)MAX_HOUSES);
 
-    for (int i = 0; i <= amount_houses; i++)
-        houses.push_back(new House());
+    for (int i = 0; i < amount_houses; i++){
+        House* random_house = new House();
+        houses.push_back(random_house);
+    }
 
-    double clipping_plane[4] = { 0.0, -1.0, 0.0, 0.0 };
-    double ratio = 0.0;
-    
-    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    glewInit();
+    glEnable(GL_DEPTH_TEST);
+    Shader shader("vertexShader.vert", "fragmentShader.frag", "geometryShader.geom");
+    shader.use();
+    glEnable(GL_CLIP_PLANE0);
+
+    // Use the shader program
+    float startFrame = static_cast<float>(glfwGetTime());
+
+    double height_clip;
 
     while (!glfwWindowShouldClose(window)) {
-        std::chrono::steady_clock::time_point between = std::chrono::steady_clock::now(); 
-        std::chrono::milliseconds::rep duration = std::chrono::duration_cast<std::chrono::milliseconds>(between - start).count(); // get time passed since render start
 
-        clipping_plane[3] = 100;
-        if (duration <= 1000){ // increase clipping plane height.
-            ratio = duration / 1000.0;
-            clipping_plane[3] = ratio * house_highest;
+        processInput(window);
+        currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        duration = currentFrame - startFrame;
+        lastFrame = currentFrame;
 
-        }else if(duration >= 5000){ // total animation cycle is over. reset clip plane height, height of the highest generated house, house parameters and timer.
-            clipping_plane[3] = 0.0;
-            std::cout << "Resetting animation." << std::endl; 
+        height_clip = get_clip_height();
+        if (duration > 5.0){
             house_highest = 0.0;
             for (House* house : houses){
                 house->set_random_dimensions();
             }
-            start = std::chrono::steady_clock::now();
-
-        }else if(duration >= 4000){ // lower the clipping plane.
-            ratio = (duration - 4000.0) / 1000.0;
-            double dist = house_highest - ratio * house_highest;
-            clipping_plane[3] = dist; 
+            startFrame = static_cast<float>(glfwGetTime());
         }
 
-        glClipPlane(GL_CLIP_PLANE0, clipping_plane);
-        glEnable(GL_CLIP_PLANE0);
-        
-        render(houses);
 
+        glm::mat4 mat_view = camera.GetViewMatrix();;
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader.setFloat("u_height_clip", height_clip);
+        for(House* house : houses){
+           shader.setFloat("u_width", house->width);
+           shader.setFloat("u_length", house->length);
+           shader.setFloat("u_height_wall", house->height_wall);
+           shader.setFloat("u_height_roof", house->height_roof);
+           shader.setVec4("u_location", house->pos_x, house->pos_y, house->pos_z, 1.0);
+           shader.setVec3("u_colour_roof", house->colour_roof_r, house->colour_roof_g, house->colour_roof_b);
+           shader.setVec3("u_colour_wall", house->colour_wall_r, house->colour_wall_g, house->colour_wall_b);
+           shader.setMat4("u_mvp", mat_projection * mat_view * house->mat_model);
+           glDrawArrays(GL_POINTS, 0, 10);
+        }
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    // Clean up
+    glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
